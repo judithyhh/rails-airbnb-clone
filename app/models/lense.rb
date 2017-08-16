da3bd8cf5910303ea50d772f5e5caaba19b789e1
Jamie Tan <jamie.enxuan@gmail.com>
2017-08-16 15:20:12 +0800

    form for adding lenses completed for now

--- app/models/lense.rb
+++ app/models/lense.rb
@@ -5,6 +5,7 @@ class Lense < ApplicationRecord
   validates :lens_type, inclusion: { in: ["Kit", "Standard", "Prime", "Telephoto Zoom", "Macro", "Wide Angle"] }
   validates :brand, inclusion: { in: ["Canon", "Leica", "Minolta", "Nikon", "Olympus"] }
   validates :condition, inclusion: { in: ["Excellent", "Good", "Usable"] }
-  validates :user_id, :price, :location, presence: true
+  validates :location, inclusion: { in: ["Chengdu", "Shanghai", "Suzhou", "Beijing", "Xinjiang", "Shenzhen", "Guangzhou"] }
+  validates :user_id, :price, presence: true
   mount_uploader :photo, PhotoUploader
 end
