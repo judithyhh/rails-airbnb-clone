63e4048346dbe2efca7b4126a65dd964d28911fd
Jasmine Tan <jasminetanom@gmail.com>
2017-08-18 12:58:25 +0800

    Most functions done.

--- app/controllers/lenses_controller.rb
+++ app/controllers/lenses_controller.rb
@@ -1,4 +1,5 @@
 class LensesController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index, :show]
   def index
     if params[:search] == "true"
       searched_lenses = Lense.where(nil)
@@ -30,35 +31,6 @@ class LensesController < ApplicationController
     end
   end
 
-  # def index
-  #   @lenses = Lense.all
-
-  #   @lenses_to_mark = Lense.where.not(latitude: nil, longitude: nil)
-
-  #   @hash = Gmaps4rails.build_markers(@lenses_to_mark) do |lense, marker|
-  #     marker.lat lense.latitude
-  #     marker.lng lense.longitude
-  #     # marker.infowindow render_to_string(partial: "/flats/map_box", locals: { flat: flat })
-  #   end
-  # end
-
-  # def search
-  #   search_lenses = Lense.where("location = ?, lens_type = ? AND brand = ? AND price < ?", params[:location], params[:lens_type], params[:brand], params[:price].to_f)
-  #   @lenses = []
-  #   search_lenses.each do |lense|
-  #     if lense.bookings.nil?
-  #       @lenses << lense
-  #     else
-  #       has_overlap_array = []
-  #       lense.bookings.each do |booking|
-  #         has_overlap = (params[:borrow_date] - booking.return_date) * (booking.borrow_date - params[:return_date]) >= 0
-  #         has_overlap_array << 1 if has_overlap
-  #       end
-  #       @lenses << lense unless has_overlap_array.include?(1)
-  #     end
-  #   end
-  # end
-
   def new
     @lense = Lense.new
   end
@@ -77,9 +49,16 @@ class LensesController < ApplicationController
   end
 
   def edit
+    @lense = Lense.find(params[:id])
   end
 
   def update
+    @lense = Lense.find(params[:id])
+    if @lense.update(lense_params)
+      redirect_to @lense
+    else
+      render :update
+    end
   end
 
   def destroy
