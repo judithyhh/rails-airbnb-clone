d31f3f448183531c6b956d4cd7a40852c9fbcee9
Jasmine Tan <jasminetanom@gmail.com>
2017-08-18 14:09:00 +0800

    Fixed bugs and improved flow.

--- app/models/lense.rb
+++ app/models/lense.rb
@@ -1,6 +1,6 @@
 class Lense < ApplicationRecord
   belongs_to :user
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
 
   validates :lens_type, inclusion: { in: ["Kit", "Standard", "Prime", "Telephoto Zoom", "Macro", "Wide Angle"] }
   validates :user, :brand, :price, :condition, :location, presence: true
