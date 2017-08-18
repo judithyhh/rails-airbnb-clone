c7e1ef8b8e42b9871e10d6ca202958cf5b78074e
Judith Hsiang <judithyhh@gmail.com>
2017-08-18 14:12:59 +0800

    took out the www in production environment.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ Rails.application.configure do
 
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: 'https://www.airbnb-judithyhh.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'https://airbnb-judithyhh.herokuapp.com' }
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
